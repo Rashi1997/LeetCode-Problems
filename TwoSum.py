3969d102b81dc7f86b01d23dc00b55a28a000838
Rashi Dhar <rdhar1997@gmail.com>
2019-05-21 20:44:55 +0530

    added two pass and one pass hash table solutions

--- TwoSum.py
+++ TwoSum.py
@@ -3,4 +3,23 @@ class BruteForce:
         for i in range(0,len(nums)):
             for j in range(i+1,len(nums)):
                 if(nums[i]+nums[j]==target):
-                    return [i,j];
+                    return [i,j];  
+class TwoPassHashTable:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashTable=dict();
+        for i in range(len(nums)):
+            hashTable[nums[i]]=i;
+        #print(hashTable);
+        for i in range(len(nums)):
+            complement=target-nums[i];
+            if((complement in nums) and  (hashTable[complement]!=i)):
+                return(i,hashTable[complement])
+class OnePassHashTable:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashTable={};
+        #print(hashTable);
+        for i in range(len(nums)):
+            complement=target-nums[i];
+            if(complement in hashTable):
+                return(hashTable[complement],i)
+            hashTable[nums[i]]=i;
